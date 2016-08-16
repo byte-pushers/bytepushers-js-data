8d0465fac663272008d33052624b30a20a13f7fd
Tonté Pouncil <pouncilt.developer@gmail.com>
2016-08-16 05:08:24 -0700

    Release v0.0.3

--- release/bytepushers-js-data.js
+++ release/bytepushers-js-data.js
@@ -1,4 +1,31 @@
-/*global window, document, BytePushers*/
+/**
+ * Created by tonte on 8/16/16.
+ */
+/*global angular, BytePushers*/
+/* jshint -W108, -W109 */
+angular.module('software.bytepushers.data.provider', []);
+angular.module('software.bytepushers.data.provider').provider('DataProvider', function () {
+    "use strict";
+    var daoConfig;
+
+    this.$get = function () {
+        if (!angular.isDefined(daoConfig)) {
+            throw new BytePushers.dao.DaoException("No DAO Configuration definded.");
+        }
+
+        BytePushers.dao.DaoManager.getInstance().registerDao(daoConfig);
+
+        return BytePushers.dao.DaoManager.getInstance();
+    };
+
+    this.setDaoConfig = function (jsonDaoConfig) {
+        daoConfig = jsonDaoConfig;
+    };
+
+    this.getDaoConfig = function () {
+        return daoConfig;
+    };
+});;/*global window, document, BytePushers*/
 /* jshint -W108, -W109 */
 
 /**
